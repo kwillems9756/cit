ad7278f8451e72da896a2298dfc40e137ecdc9f4
CodeBoy124 <koen7maart@icloud.com>
2023-12-05 09:04:23 +0100

    Moved templates into subfolders and added config file to templates

--- src/command/setup.ts
+++ src/command/setup.ts
@@ -13,9 +13,25 @@ export default function setup(filename: string) {
     const templateRootFolder = joinPath(cwd, "templates");
     makeTemplatesFolderIfNeeded(templateRootFolder);
 
-    // TODO: Allow extension to be added to filename
-    const templateFilePath = joinPath(templateRootFolder, filename + ".ejs");
-    const placeholderText = "Place here your EJS template code";
+    // TODO: Warning if sub folder already exists
+    const templateSubFolder = joinPath(templateRootFolder, filename);
+    makeTemplatesFolderIfNeeded(templateSubFolder);
 
+    const templateFilePath = joinPath(templateSubFolder, "template.ejs");
+    const placeholderText = "Place here your EJS template code";
     writeFileSync(templateFilePath, placeholderText);
+
+    const configFilePath = joinPath(templateSubFolder, "config.json");
+    const placeholderConfigText = JSON.stringify({
+        description: "Some placeholder description",
+        arguments: {
+            isPlaceholder: {
+                type: "boolean",
+                default: true,
+                description: "Some placeholder argument description"
+            }
+        },
+        version: "1.0.0",
+    }, null, 2);
+    writeFileSync(configFilePath, placeholderConfigText);
 }
