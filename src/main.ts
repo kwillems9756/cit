22ed642528bce50730cae8bbeabab26208225dfe
CodeBoy124 <koen7maart@icloud.com>
2023-12-05 11:36:33 +0100

    Implement info command

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { program } from "commander";
 
 import { name, version, description } from "../package.json";
+import info from "./command/info";
 
 program
     .name(name)
     .description(description)
     .version(version);
+
+program
+    .command('info')
+    .description('Get information about a template')
+    .argument('<template>')
+    .action(info)
 
 program.parse();
