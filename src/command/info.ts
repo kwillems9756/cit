7e797552a0cd32fc46adbed997430bd10d6d0c6a
CodeBoy124 <koen7maart@icloud.com>
2023-12-06 09:36:09 +0100

    Added validation to info command

--- src/command/info.ts
+++ src/command/info.ts
@@ -1,19 +1,12 @@
 import { existsSync, readFileSync } from "fs"
 import { join as joinPath } from "path"
 import type { templateConfig } from "./types"
+import { checkTemplate } from "./util"
 
 export default function info(templateName: string) {
-    const templatePath = joinPath(process.cwd(), "templates", templateName)
-    if(!existsSync(templatePath)) {
-        console.error('Error: Template not found')
-        process.exit(1)
-    }
-    
-    const infoPath = joinPath(templatePath, "config.json");
-    if(!existsSync(infoPath)) {
-        console.error('Error: Template not configured correctly, no config.json file found')
-        process.exit(1)
-    }
+    checkTemplate(templateName)
+
+    const infoPath = joinPath(process.cwd(), "templates", templateName, "config.json")
 
     const config: templateConfig = JSON.parse(readFileSync(infoPath, 'utf8'))
     console.log(`Version: ${config.version}\nUsage: cit make --use=${templateName} someFileToCreate\n\n${config.description}\n\nArguments:${
